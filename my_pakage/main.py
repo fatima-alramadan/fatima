e49bc254a1614d1570bd2522287e8298b6be430d
fatima-alramadan <103146742+fatima-alramadan@users.noreply.github.com>
2022-08-01 21:59:54 +0300

    Update main.py

--- my_pakage/main.py
+++ my_pakage/main.py
@@ -1,72 +1,82 @@
 import numpy as np
 
 class myClass:
-    def __init__(self,arrList):
+    def __init__(self, arrList=[1,2,3,4,5,6]):
         self.arrList = arrList
     
-    def return_max(arrList):
+    def return_max(self):
 
-        maxVal = arrList[0]
+        maxVal = self.arrList[0]
     
-        for num in arrList:
+        for num in self.arrList:
             if num > maxVal:
                 maxVal = num
             
         return maxVal
 
-    def return_min(arrList):
-        minVal = arrList[0]
+    def return_min(self):
+        minVal = self.arrList[0]
     
-        for num in arrList:
+        for num in self.arrList:
             if num < minVal:
                 minVal = num
             
-    return minVal
+        return minVal
 
-    def return_max_squared(arrList):
-        maxVal = arrList[0]
+    def return_max_squared(self):
+        maxValue = self.arrList[0]
     
-        for num in arrList:
-            if num > maxVal:
-                maxVal = num
+        for num in self.arrList:
+            if num > maxValue:
+                maxValue = num
 
-        return maxVal* maxVal
+        return maxValue*maxValue
 
-    def return_length(arrList):
+    def return_length(self):
          count = 0
-        for l in arrList:
+         for l in self.arrList:
             count += 1
-        return count
+         return count
 
-    def return_positive_sum(arrList):
-          posSum = 0
-        for num in arrList:
+    def return_positive_sum(self):
+        posSum = 0
+        for num in self.arrList:
             if num > 0 :
                 posSum += num
                 
-        arrList[len(arrList)-1] = posSum
+        self.arrList[len(self.arrList)-1] = posSum
         
-        return arrList
+        return posSum
 
-    def return_negative_count(arrList):
+    def return_negative_count(self):
         counter = 0
-        for num in arrList:
+        for num in self.arrList:
             if num < 0 :
                 counter += 1
 
-        return arrList
+        return counter
+ 
 
-    def sum(arrList):
-        sum1 = 0
-        for num in arrList:
-            sum1 += num
-        return sum1  
+    def positive_count(self):
+        count = 0
+        for num in self.arrList:
+            if num > 0 :
+                count += 1
 
-    def average(arrList):
-        sum1 = 0
-        for n in arrList:
-            sum1 += n
-        return (sum1/len(arrList))  
+        return count
+
+
+        return 
+
+    def negative_sum(self):
+        negSum = 0
+        for num in self.arrList:
+            if num < 0 :
+                negSum += num
+                
+        self.arrList[len(self.arrList)-1] = negSum
+        
+        return negSum
     
 
 if __name__ == "__main__":
